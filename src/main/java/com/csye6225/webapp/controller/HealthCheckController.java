c0972721b846d73733de790959e1fe856bc6a11e
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2024-02-04 01:57:21 -0500

    Added Assignment-1 code

---
+++ src/main/java/com/csye6225/webapp/controller/HealthCheckController.java
@@ -0,0 +1,67 @@
+package com.csye6225.webapp.controller;
+
+import com.csye6225.webapp.service.DatabaseHealthCheckService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/healthz")
+public class HealthCheckController {
+    DatabaseHealthCheckService databaseHealthCheckService;
+
+    public HealthCheckController(DatabaseHealthCheckService databaseHealthCheckService) {
+        this.databaseHealthCheckService = databaseHealthCheckService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> healthCheck(@RequestParam Map<String, String> queryParams, @RequestBody(required = false) String requestBody) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().mustRevalidate());
+        headers.setPragma("no-cache");
+        headers.add("X-Content-Type-Options", "nosniff");
+
+        if((queryParams != null && !queryParams.isEmpty()) ||
+                (requestBody != null && !requestBody.isEmpty()))
+        {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .headers(headers)
+                    .build();
+        }
+
+        boolean isDatabaseConnected = databaseHealthCheckService.checkDatabaseConnection();
+
+        if(isDatabaseConnected)
+        {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .headers(headers)
+                    .build();
+        }
+        else
+        {
+            return ResponseEntity
+                    .status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .headers(headers)
+                    .build();
+        }
+    }
+
+    @RequestMapping(method = {RequestMethod.HEAD, RequestMethod.OPTIONS})
+    public ResponseEntity<Void> handleHeadAndOptionsMethods() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().mustRevalidate());
+        headers.setPragma("no-cache");
+        headers.add("X-Content-Type-Options", "nosniff");
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .headers(headers)
+                .build();
+    }
+}
---
+++ src/main/java/com/csye6225/webapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.csye6225.webapp.exception;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    HttpHeaders headers;
+
+    public GlobalExceptionHandler() {
+        this.headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().mustRevalidate());
+        headers.setPragma("no-cache");
+        headers.add("X-Content-Type-Options", "nosniff");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class) // For 405 status code - Method not allowed
+    public ResponseEntity<Void> handleMethodNotSupported() {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .headers(headers)
+                .build();
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class) // For 404 status code - Not Found
+    public ResponseEntity<Void> handleNotFoundException() {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .headers(headers)
+                .build();
+    }
+}
