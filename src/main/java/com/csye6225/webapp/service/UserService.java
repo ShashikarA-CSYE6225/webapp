a378f535378d0880599c1e59e90882125f8587d8
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2024-02-07 01:42:32 -0500

    Modifed PUT to allow for updating any field

--- src/main/java/com/csye6225/webapp/service/UserService.java
+++ src/main/java/com/csye6225/webapp/service/UserService.java
@@ -2,6 +2,7 @@ package com.csye6225.webapp.service;
 
 import com.csye6225.webapp.dto.UserResponseDto;
 import com.csye6225.webapp.exception.IncorrectPasswordException;
+import com.csye6225.webapp.exception.InvalidAuthorizationException;
 import com.csye6225.webapp.exception.UserNotFoundException;
 import com.csye6225.webapp.exception.UsernameAlreadyExistsException;
 import com.csye6225.webapp.model.User;
@@ -10,15 +11,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    public UserResponseDto createUser(User user) throws UsernameAlreadyExistsException;
+    public UserResponseDto createUser(User user, String auth) throws UsernameAlreadyExistsException, InvalidAuthorizationException;
 
     public List<UserResponseDto> getAllUsers();
 
-    public UserResponseDto updateUser(String basicAuth, User requestBody) throws UserNotFoundException, IncorrectPasswordException;
+    public UserResponseDto updateUser(String basicAuth, User requestBody) throws UserNotFoundException, IncorrectPasswordException, InvalidAuthorizationException;
 
     public Optional<UserResponseDto> findByUserNameAndPassword(String userName, String password);
 
-    public UserResponseDto getUser(User requestBody, String userName) throws UserNotFoundException, IncorrectPasswordException;
+    public UserResponseDto getUser(User requestBody, String userName) throws UserNotFoundException, IncorrectPasswordException, InvalidAuthorizationException;
 
     public UserResponseDto mapToDto(User user);
 }
