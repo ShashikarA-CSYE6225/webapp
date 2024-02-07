047057431641464305d6ddafe8f3b7e07caa5688
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2024-02-07 17:18:57 -0500

    Added github secrets for workflow

---
+++ src/test/java/com/csye6225/webapp/WebAppTests.java
@@ -0,0 +1,12 @@
+package com.csye6225.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
