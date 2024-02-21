44179f5c1dea8dd339b48839480a3dd7b23a90f5
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2024-02-21 15:43:14 -0500

    Removed java path

--- packer/scripts/install_java_sql.sh
+++ packer/scripts/install_java_sql.sh
@@ -8,21 +8,12 @@ sudo systemctl start mysqld
 sleep 5
 
 # Set MySQL root password
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Sqlpassword1.';"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
 echo "MySQL installation and root password setup completed."
 
 # Install OpenJDK 17
-sudo dnf -y install java-17-openjdk java-17-openjdk-devel
-
-# Set up environment variables for Java
-cat > /etc/profile.d/java.sh <<'EOF'
-export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which java)))))
-export PATH=$PATH:$JAVA_HOME/bin
-EOF
-
-# Source the profile script to apply changes
-source /etc/profile.d/java.sh
+sudo dnf install java-17-openjdk-devel.x86_64 -y
 
 # Verify Java installation
 java --version
