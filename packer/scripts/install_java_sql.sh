6bd334b81fd39e36ad161dd8f098e0954f6fc7d3
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2024-02-21 19:14:11 -0500

    Added installed sql

--- packer/scripts/install_java_sql.sh
+++ packer/scripts/install_java_sql.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-# Install MySQL and start service
-sudo dnf install -y mysql mysql-server
-sudo systemctl start mysqld
-
-# Wait for MySQL service to start
-sleep 5
-
-# Set MySQL root password
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
-
-echo "MySQL installation and root password setup completed."
-
-# Install OpenJDK 17
 sudo dnf install java-17-openjdk-devel.x86_64 -y
 
-# Verify Java installation
-java --version
+sudo dnf install mysql mysql-server -y
 
-echo "Java installation completed."
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
+
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
